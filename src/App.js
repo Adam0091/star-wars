51060c2366a0b93233cabf7cd9145455cb332526
Adam <adam.holo@mail.ru>
2022-05-27 19:05:22 +0400

    refacor: add required directories

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-const App = () => {
-  return (
-    <div>Хай</div>
-  )
-}
-
-export default App;
---
+++ src/containers/App/App.jsx
@@ -0,0 +1,9 @@
+import styles from "./App.module.css";
+
+import { getApiResource } from '../../utils/network';
+
+const App = () => {
+  return (<div className={styles.header}>Хай</div>);
+};
+
+export default App;
