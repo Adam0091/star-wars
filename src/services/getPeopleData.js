3ddf1dc648042162eb69ee3704af9e44a00a0852
Adam <adam.holo@mail.ru>
2022-06-05 21:38:22 +0400

    feat: add getPeopleId and getPeopleImage

---
+++ src/services/getPeopleData.js
@@ -0,0 +1,17 @@
+import {
+  HTTPS,
+  SWAPI_ROOT,
+  SWAPI_PEOPLE,
+  GUIDE_IMG_EXTENSION,
+  URL_IMG_PERSON,
+} from "../constants/api";
+
+const getID = (url, category) => {
+  const id = url.replace(HTTPS + SWAPI_ROOT + category, "").replace(/\//g, "");
+  return id;
+};
+
+export const getPeopleId = (url) => getID(url, SWAPI_PEOPLE);
+
+export const getPeopleImage = (id) =>
+  `${URL_IMG_PERSON}/${id + GUIDE_IMG_EXTENSION}`;
