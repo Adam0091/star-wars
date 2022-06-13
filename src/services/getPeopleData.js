172c9df09fc031c245ed352745c5ad37a1ec9716
Adam <adam.holo@mail.ru>
2022-06-13 16:45:21 +0400

    feat: add service getPeoplePageId

--- src/services/getPeopleData.js
+++ src/services/getPeopleData.js
@@ -2,6 +2,7 @@ import {
   HTTPS,
   SWAPI_ROOT,
   SWAPI_PEOPLE,
+  SWAPI_PEOPLE_PAGE,
   GUIDE_IMG_EXTENSION,
   URL_IMG_PERSON,
 } from "@constants/api";
@@ -15,3 +16,10 @@ export const getPeopleId = (url) => getID(url, SWAPI_PEOPLE);
 
 export const getPeopleImage = (id) =>
   `${URL_IMG_PERSON}/${id + GUIDE_IMG_EXTENSION}`;
+
+export const getPeoplePageId = (url) => {
+  const pos = url.lastIndexOf(SWAPI_PEOPLE_PAGE);
+  const id = url.slice(pos + SWAPI_PEOPLE_PAGE.length, url.length);
+
+  return Number(id);
+};
