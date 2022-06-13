00eda96b798cedb7f1f3ebfd599b5cc3bfd7b588
Adam <adam.holo@mail.ru>
2022-06-13 16:48:16 +0400

    feat: add routesCongif for pages

---
+++ src/routes/routesConfig.js
@@ -0,0 +1,23 @@
+import PeoplePage from "@containers/PeoplePage/";
+import HomePage from "@containers/HomePage/";
+import NotFoundPage from "@containers/NotFoundPage/";
+
+const routesConfig = [
+  {
+    path: "/",
+    exact: true,
+    element: <HomePage />,
+  },
+  {
+    path: "/people",
+    exact: true,
+    element: <PeoplePage />,
+  },
+  {
+    path: "*",
+    exact: false,
+    element: <NotFoundPage />,
+  },
+];
+
+export default routesConfig;
