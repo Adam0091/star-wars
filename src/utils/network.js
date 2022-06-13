9fb29f08ecd8d1c0a004e22a524f3c4a1657c21a
Adam <adam.holo@mail.ru>
2022-06-13 16:46:33 +0400

    feat: add function changeHTTP  and commits for function

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,3 +1,22 @@
+import { HTTP, HTTPS } from "@constants/api";
+
+/**
+ * Изменяет url с HTTP на HTTPS
+ * @param {String} url - url для изменения
+ * @returns {String} - url c HTTPS
+ */
+
+export const changeHTTP = (url) => {
+  const result = url ? url.replace(HTTP, HTTPS) : url;
+  return result;
+};
+
+/**
+ * Отправлет запрос fetch принимает url
+ * @param {String} url - для запроса
+ * @returns {Promise} - Promise результатом запроса
+ */
+
 export const getApiResource = async (url) => {
   try {
     const res = await fetch(url);
